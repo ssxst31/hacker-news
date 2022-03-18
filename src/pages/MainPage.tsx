7a51d87d993309c27afb44f53344637753499691
star <chlvkfksquf@gmail.com>
2022-03-19 03:23:16 +0900

    Add categoryList layOut

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,23 @@ import React from 'react';
 import Button from '../components/Button';
 
 function MainPage() {
-  return <div className="w-full bg-yellow-500"></div>;
+  const categoryList = [
+    'topstories',
+    'newstories',
+    'askstories',
+    'showstories',
+    'jobstories',
+  ];
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      {categoryList.map((item, index) => (
+        <div className="mx-4">
+          <Button text={item} key={index} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default MainPage;
