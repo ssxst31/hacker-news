ebb77bdd18e5ede31c9805edb8aac96d6c30b233
star <chlvkfksquf@gmail.com>
2022-03-19 02:15:55 +0900

    Add axios

---
+++ src/api/axios.tsx
@@ -0,0 +1,9 @@
+import Axios from 'axios';
+
+export const axios = Axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+export const get = (url: string) => {
+    return axios.get(url);
+};
