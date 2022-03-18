76132312e8028b2f2a8e79e608e16aa545dce222
star <chlvkfksquf@gmail.com>
2022-03-19 03:53:16 +0900

    Add Link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 interface ButtonProps {
   text: string;
 }
 
 function Button({ text }: ButtonProps) {
   return (
-    <button
-      type="button"
-      className="w-48 h-10 bg-yellow-500 text-center text-2xl"
-    >
-      {text}
-    </button>
+    <Link to={`/${text}`}>
+      <button
+        type="button"
+        className="w-48 h-10 bg-yellow-500 text-center text-2xl"
+      >
+        {text}
+      </button>
+    </Link>
   );
 }
 
