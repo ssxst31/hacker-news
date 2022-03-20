20b598dac58e6454fd18d0319d3a9c095f2f684d
star <chlvkfksquf@gmail.com>
2022-03-20 20:46:59 +0900

    getCategoryDetail

--- src/api/category/index.tsx
+++ src/api/category/index.tsx
@@ -1,5 +1,9 @@
 import { get } from '../axios';
 
 export const getCategory = (category: string) => {
-    return get(`/${category}.json?print=pretty`);
+  return get(`/${category}.json?print=pretty`);
+};
+
+export const getCategoryDetail = (id: number) => {
+  return get(`/item/${id}​.json?print=pretty`);
 };
